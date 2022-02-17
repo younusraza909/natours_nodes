9e8fdc319e8e2f301615d76f85f2555c18f873ad
younusraza <younusraza909@gmail.com>
2022-02-17 23:18:58 +0500

    bug fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -43,7 +43,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   })
 })
 
-exports.delete = catchAsync(async (req, res, next) => {
+exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false })
 
   res.status(204).json({
